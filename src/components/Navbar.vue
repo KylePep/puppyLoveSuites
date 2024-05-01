2999b0064115f0e7294cbaf877e9763d247b00e2
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-01 03:45:36 -0600

    Cutting down template

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -32,6 +32,16 @@ function toggleTheme() {
       <ul class="navbar-nav me-auto">
         <li>
           <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+            Contact Us
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+            Boarding
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
             About
           </router-link>
         </li>
