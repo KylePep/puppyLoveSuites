b6a5e26dceded429755503608112ab3542286126
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-01 03:36:45 -0600

    init repo

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    This site is under construction
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: clamp(500px, 50vw, 100%);
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
