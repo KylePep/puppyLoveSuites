332dbdaadea36fbfb6483b5bd24fbdefcb9e39c2
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-01 05:37:23 -0600

    work on contactus modal

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -14,15 +14,23 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="contactModalLabel">Modal title</h1>
+          <h1 class="modal-title fw-bold fs-5" id="contactModalLabel">Contact Us</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <ul>
+            <li><span class="mdi mdi-home-variant fw-semibold"> Address:</span> 10330 Lake Shore Dr, Nampa, ID 83686
+            </li>
+            <li><span class="mdi mdi-clock fw-semibold"> Hours:</span> {{ new Date().toDateString() }}</li>
+            <li><span class="mdi mdi-phone fw-semibold"> Phone:</span> (208) 353-5939</li>
+            <li><span class="mdi mdi-email fw-semibold"> Email:</span> puppylovesuites@gmail.com</li>
+          </ul>
+          <p class="mx-5">
+            Heres a blerb about contacting Puppy Love Suites.
+          </p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
@@ -31,4 +39,12 @@
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+ul {
+  list-style-type: none;
+}
+
+ul>li {
+  margin-top: 1rem;
+}
+</style>
