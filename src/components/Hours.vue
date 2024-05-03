768b531ebf1bde198dad96b68115ebe929f76644
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-03 11:55:40 -0600

    Tinkering with Hours component

---
+++ src/components/Hours.vue
@@ -0,0 +1,72 @@
+<template>
+  <div v-if="isSmallScreen" class="d-flex flex-column justify-content-center align-items-center">
+    <div v-for="day in longDays" :key="day"
+      :class="[day.split('|')[0] == 'Sunday' || day.split('|')[0] == 'Saturday' ? 'bg-light' : 'bg-white', day.split('|')[0] == 'Sunday' ? 'border-bottom-1 rounded-top-1' : 'border-top-0', day.split('|')[0] == 'Saturday' ? 'rounded-bottom-1' : '']"
+      class="d-flex flex-column border border-black w-50 text-center">
+      <p class="bg-primary text-white fw-bold px-2 py-2 mb-0">{{ day.split('|')[0] }}</p>
+
+      <div class="d-flex flex-row justify-content-center py-2">
+
+        <div class="px-2">{{
+    day.split('|')[1].split('-')[0] }}</div>
+        <i v-if="day != 'Sunday|Closed'">-</i>
+        <div v-if="day != 'Sunday|Closed'" class="px-2">{{ day.split('|')[1].split('-')[1] }}</div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div v-else class="d-flex flex-column justify-content-center align-items-center">
+    <div class="d-flex flex-row w-75">
+
+      <div v-for="day in longDays" :key="day"
+        :class="[day.split('|')[0] == 'Sunday' || day.split('|')[0] == 'Saturday' ? 'bg-light' : 'bg-white', day.split('|')[0] == 'Sunday' ? 'border-start-1 rounded-start' : 'border-start-0', day.split('|')[0] == 'Saturday' ? 'rounded-end' : '']"
+        class="d-flex flex-column flex-grow-1 border border-black text-center">
+        <div class="bg-primary text-white fw-bold px-2 py-2">{{ day.split('|')[0] }}</div>
+
+        <div class="d-flex flex-column flex-grow-1 ">
+          <div class="px-2 ">{{
+    day.split('|')[1].split('-')[0] }}</div>
+          <i v-if="day != 'Sunday|Closed'" class="fw-semibold">-</i>
+          <div v-if="day != 'Sunday|Closed'" class="px-2">{{ day.split('|')[1].split('-')[1] }} </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {
+      screenWidth: window.innerWidth,
+      shortDays: ["Sun|Closed", "Mon|8AM-6PM", "Tue|8AM-6PM", "Wed|8AM-6PM", "Thu|8AM-6PM", "Fri|8AM-6PM", "Sat|9AM-12PM"],
+      longDays: ["Sunday|Closed", "Monday|8AM-6PM", "Tuesday|8AM-6PM", "Wednesday|8AM-6PM", "Thursday|8AM-6PM", "Friday|8AM-6PM", "Saturday|9AM-12PM"]
+    }
+  },
+  computed: {
+    isSmallScreen() {
+      return this.screenWidth < 768;
+    }
+  },
+  mounted() {
+    // Update screenWidth when the window is resized
+    window.addEventListener('resize', this.updateScreenWidth);
+  },
+  methods: {
+    updateScreenWidth() {
+      this.screenWidth = window.innerWidth;
+    }
+  },
+  beforeUnmount() {
+    // Remove event listener to prevent memory leaks
+    window.removeEventListener('resize', this.updateScreenWidth);
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,7 +1,17 @@
-<script setup>
-const days = ["Sun|Closed", "Mon|8AM-6PM", "Tue|8AM-6PM", "Wed|8AM-6PM", "Thu|8AM-6PM", "Fri|8AM-6PM", "Sat|9AM-12PM"]
-const weekends = ["Sun|Closed", "Sat|9AM-12PM",]
+<script>
+import { onMounted } from "vue";
 
+
+
+export default {
+  data() {
+    return {
+
+    };
+
+  },
+
+};
 </script>
 
 <template>
@@ -15,22 +25,13 @@ const weekends = ["Sun|Closed", "Sat|9AM-12PM",]
 
     <div class="d-flex flex-column">
       <h2 class="mdi mdi-clock fw-semibold"> Hours: </h2>
-      <div class="d-flex flex-row justify-content-center">
-        <div v-for="day in days" :key="day"
-          :class="[day.split('|')[0] == 'Sun' || day.split('|')[0] == 'Sat' ? 'bg-gray' : 'bg-white', day.split('|')[0] == 'Sun' ? 'border-start-1 rounded-start' : 'border-start-0', day.split('|')[0] == 'Sat' ? 'rounded-end' : '']"
-          class="d-flex flex-column border border-black text-center">
-          <p class="bg-primary text-white px-2 py-1">{{ day.split('|')[0] }}</p>
-          <div class="d-flex flex-column flex-sm-row">
-            <p class="px-2 mb-0">{{ day.split('|')[1].split('-')[0] }}</p>
-            <i v-if="day != 'Sun|Closed'">-</i>
-            <p class="px-2">{{ day.split('|')[1].split('-')[1] }}</p>
-          </div>
-        </div>
-      </div>
+
+      <Hours />
+
     </div>
 
     <div class="d-flex justify-content-center">
-      <i class="separationMargin border-bottom border-2 border-primary w-75 my-5 align-items-center"> </i>
+      <i class="separationMargin border-bottom border-2 border-primary w-75 align-items-center"> </i>
     </div>
 
     <div>
@@ -38,12 +39,12 @@ const weekends = ["Sun|Closed", "Sat|9AM-12PM",]
       </h2>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2893.605939266819!2d-116.59967929999998!3d43.51055549999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae49b3d5ccf939%3A0x7870a107ef3ae032!2sPuppy%20Love%20Suites!5e0!3m2!1sen!2sus!4v1714639389907!5m2!1sen!2sus"
-        width="300" height="225" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        width="75%" height="225" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <p>10330 Lake Shore Dr, Nampa, ID 83686</p>
     </div>
 
     <div class="d-flex justify-content-center">
-      <i class="border-bottom border-2 border-primary w-75 my-5 align-items-center"> </i>
+      <i class="separationMargin border-bottom border-2 border-primary w-75 align-items-center"> </i>
     </div>
 
 
@@ -54,7 +55,7 @@ const weekends = ["Sun|Closed", "Sat|9AM-12PM",]
     </div>
 
     <div class="d-flex justify-content-center">
-      <i class="border-bottom border-2 border-primary w-75 my-5 align-items-center"> </i>
+      <i class="separationMargin border-bottom border-2 border-primary w-75 align-items-center"> </i>
     </div>
 
 
@@ -76,11 +77,13 @@ article>div {
 
 .Hero {
   background-image: url(https://images.unsplash.com/photo-1586095088311-dc72701cc9cc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
-  background-position: bottom;
+  background-position: 95%;
   background-size: cover;
-  background-repeat: no-repeat;
+  background-rpeat: no-repeat;
   min-height: 10rem;
 }
 
-.separationMargin {}
+.separationMargin {
+  margin: 2rem 0 2rem 0;
+}
 </style>
