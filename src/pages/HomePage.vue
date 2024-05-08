57ad5fb54639df64c645670cf1cf4db4e4be966f
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-08 03:50:00 -0600

    refactored highlights

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -21,10 +21,10 @@ import Notification from "../components/Notification.vue";
     </div>
   </div>
 
-  <article class="row g-2 my-3">
+  <article class="row position-relative g-2 my-3">
 
     <!-- <Notification /> -->
-    <div class="row">
+    <div class="row mb-3">
       <div class="col-6 px-0 d-flex justify-content-end align-items-center">
         <router-link :to="{ name: 'About' }"
           class="btn text-white fw-bold bg-primary px-3 py-1 rounded-4 selectable text-uppercase">
@@ -42,27 +42,52 @@ import Notification from "../components/Notification.vue";
 
     <PhotoCarousel />
 
-    <div class="col-12 d-flex justify-content-center">
+    <div class="col-12 my-3 d-flex justify-content-center">
       <ContactUs />
     </div>
 
-    <div class="row my-4 p-0">
-      <div class="col-3 m-auto highlight">
-        <h3 class=" ">Play</h3>
-        <h4>blerb</h4>
-        <p>Solo and group play</p>
-      </div>
-      <div class="col-3 m-auto highlight">
-        <h3 class=" ">Sleep</h3>
-        <h4>blerb</h4>
+    <!-- Need to stress test these, will likely break uniformity -->
+    <div class="row px-0 my-0 g-3 highlight-row">
 
-        <p>Individual headed and cooled suites</p>
+      <div class="col-6 col-md-4 highlight">
+        <div>
+          <h3 class=" ">Eat</h3>
+          <h4>Scheduled feeding times</h4>
+          <p>Individual feeding respecting dietary needs</p>
+        </div>
       </div>
-      <div class="col-3 m-auto highlight">
-        <h3 class=" ">Eat</h3>
-        <h4>blerb</h4>
-        <p>Individual feeding respecting dietary needs</p>
+
+      <div class="col-6 col-md-4  highlight">
+        <div>
+          <h3 class="">Play</h3>
+          <h4>Group play & Solo play</h4>
+          <p>Personalized attention, and plenty of playtime in our secure,
+            supervised indoor and outdoor areas. </p>
+        </div>
       </div>
+
+      <div class="col-6 col-md-4 mx-auto highlight">
+        <div>
+          <h3 class=" ">Sleep</h3>
+          <h4>Individual Suites</h4>
+          <p>Our luxury suites offer a cozy retreat for your beloved companion, complete with heated floors in the
+            chilly
+            months and air conditioning in the summer.
+          </p>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <p class="text-center fs-4">Beyond exceptional care, we also strive to create a welcoming and
+          inclusive
+          environment
+          for both dogs and
+          their
+          humans. We believe in building relationships with our clients based on trust, transparency, and genuine
+          compassion
+          for animals. </p>
+      </div>
+
     </div>
 
     <!-- <div class="col-12">
@@ -109,33 +134,55 @@ import Notification from "../components/Notification.vue";
   min-height: 10rem;
 }
 
+.highlight-row {
+  margin: 1rem 0rem;
+}
+
 .highlight {
-  background-color: white;
-  margin: 0rem 1rem 0rem 1rem;
-  padding: 0;
+  margin: 0;
 
-  >h3 {
-    background-color: var(--bs-secondary);
-    color: var(--bs-light);
-    font-size: large;
-    text-align: center;
-    margin-bottom: 0;
-    padding: 1rem 0rem 1rem 0rem;
+  >div {
+    min-height: 256px;
+    border-radius: 6px 6px 6px 6px;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 2px 2px 6px rgb(120, 129, 142);
+
+
+    >h3 {
+      background-color: var(--bs-secondary);
+      color: var(--bs-light);
+      font-size: large;
+      font-weight: bold;
+      text-align: center;
+      margin-bottom: 0;
+      padding: 1rem 0rem .25rem 0rem;
+      border-radius: 6px 6px 0 0;
+
+    }
+
+    >h4 {
+      background-color: var(--bs-secondary);
+      color: var(--bs-light);
+      font-size: x-small;
+      text-align: center;
+      margin: 0;
+      padding: 0rem 0rem 1rem 0rem;
+
+    }
+
+    >p {
+      flex-grow: 1;
+      color: var(--bs-secondary);
+      margin: 0;
+      padding: .5rem 1rem 1rem 1rem;
+      font-size: smaller;
+
+      background-color: white;
+    }
   }
 
-  >h4 {
-    background-color: var(--bs-secondary);
-    color: var(--bs-light);
-    font-size: small;
-    text-align: center;
 
-    padding: 0rem 0rem 1rem 0rem;
-  }
-
-  >p {
-    color: var(--bs-secondary);
-    padding: 0rem 1rem 1rem 1rem;
-  }
 }
 
 .dismiss:hover {
